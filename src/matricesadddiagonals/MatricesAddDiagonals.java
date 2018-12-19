0a046355f518440b0230b79a165a7467c3eb23a5
tsfapps <appslelo.com@gmail.com>
2018-12-19 10:03:47 +0530

    Output Added

--- src/matricesadddiagonals/MatricesAddDiagonals.java
+++ src/matricesadddiagonals/MatricesAddDiagonals.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package matricesadddiagonals;
 
 
@@ -61,6 +57,7 @@ class MatricesAddDiagonals {
 
  /*Output
 
+Logic
 1
 7
 18
